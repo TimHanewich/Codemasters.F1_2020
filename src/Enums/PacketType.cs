282fecaf87534964f8f06a7c45514ae24ce79b91
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-13 09:14:11 -0700

    Added package sizes to the packet type enum

--- src/Enums/PacketType.cs
+++ src/Enums/PacketType.cs
@@ -4,15 +4,15 @@ namespace Codemasters.F1_2020
 {
     public enum PacketType
     {
-        Motion,
-        Session,
-        Lap,
-        Event,
-        Participants,
-        CarSetup,
-        CarTelemetry,
-        CarStatus,
-        FinalClassification, //New to F1 2020
-        LobbyInfo //New to F1 2020
+        Motion, //1464 bytes
+        Session, //251 bytes
+        Lap, //1190 bytes
+        Event, //35 bytes
+        Participants, //1213 bytes
+        CarSetup, //1102 bytes
+        CarTelemetry, //1307 bytes
+        CarStatus, //1344 bytes
+        FinalClassification, //839 bytes    New to F1 2020.
+        LobbyInfo //1169 bytes   New to F1 2020
     }
 }
