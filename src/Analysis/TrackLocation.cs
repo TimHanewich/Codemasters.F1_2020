ace650579290b8d02a292cdea07524ea00d891d2
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-16 19:44:34 -0700

    Added Track locaiton

---
+++ src/Analysis/TrackLocation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Codemasters.F1_2020.Analysis
+{
+    public class TrackLocation
+    {
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public float PositionZ { get; set; }
+        public byte Sector { get; set; }
+    }
+}
