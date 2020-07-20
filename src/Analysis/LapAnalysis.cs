1508587184543aa253314def0b6b1b779514c44d
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-20 10:17:24 -0700

    Made LapAnalysis neater

--- src/Analysis/LapAnalysis.cs
+++ src/Analysis/LapAnalysis.cs
@@ -11,25 +11,18 @@ namespace Codemasters.F1_2020.Analysis
         public float Sector3Time {get; set;}
         public float LapTime {get; set;}
         public bool LapInvalid {get; set;}
-
         public float FuelConsumed {get; set;}
-        
         public float PercentOnThrottle {get; set;}
         public float PercentOnBrake {get; set;}
         public float PercentCoasting {get; set;}
         public float PercentThrottleBrakeOverlap {get; set;}
         public float PercentOnMaxThrottle {get; set;}
         public float PercentOnMaxBrake {get; set;}
-
         public float ErsDeployed {get; set;}
         public float ErsHarvested {get; set;}
-
         public int GearChanges {get; set;}
-
         public ushort TopSpeedKph {get; set;}
         public ushort TopSpeedMph {get; set;}
-
         public float IncrementalAverageTyreWear {get; set;}
-
     }
 }
