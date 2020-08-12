9c3c9f6ac795a6a6cf68ecbbdc867bf021af15d3
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-11 18:49:01 -0700

    Added optimum values to TrackLocation

--- src/Analysis/TrackLocation.cs
+++ src/Analysis/TrackLocation.cs
@@ -4,9 +4,17 @@ namespace Codemasters.F1_2020.Analysis
 {
     public class TrackLocation
     {
+        //Basic info
         public float PositionX { get; set; }
         public float PositionY { get; set; }
         public float PositionZ { get; set; }
         public byte Sector { get; set; }
+
+        //Optimums
+        public float OptimalSpeedMph {get; set;} //Probably a major point of interest
+        public sbyte OptimalGear {get; set;} //Probably a major point of interest
+        public float OptimalSteer {get; set;}
+        public float OptimalThrottle {get; set;}
+        public float OptimalBrake {get; set;}
     }
 }
