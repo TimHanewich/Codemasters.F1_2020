e4b624a63e79e4f28ba6e183e2158221d925f3cc
Tim Hanewich <tahanewich@live.com>
2020-09-14 17:02:09 -0700

    Testing

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -13,10 +13,19 @@ namespace FunctionalTesting
     {
         static void Main(string[] args)
         {
-            TrackDataContainer tdc = TrackDataContainer.LoadTrack(Track.Silverstone);
-            Console.WriteLine(JsonConvert.SerializeObject(tdc));
+            string path = "C:\\Users\\TaHan\\Downloads\\Codemasters.F1_2020\\SampleData\\Australia_Practice_AlphaTauri.json";
+            string content = System.IO.File.ReadAllText(path);
 
+            List<byte[]> bytes = JsonConvert.DeserializeObject<List<byte[]>>(content);
+            Packet[] packets = Packet.BulkLoadAllSessionData(bytes);
+            SessionAnalysis sa = new SessionAnalysis();
+            Console.WriteLine("Loading...");
+            sa.Load(packets, packets[0].PlayerCarIndex);
 
+            foreach (LapAnalysis la in sa.Laps)
+            {
+                Console.WriteLine(la.EquippedTyreCompound.ToString() + " - " + la.IncrementalTyreWear_FrontLeft.ToString() + " " + la.IncrementalTyreWear_FrontRight.ToString() + " " + la.IncrementalTyreWear_RearLeft.ToString() + " " + la.IncrementalTyreWear_RearRight.ToString());
+            }
         }
     }
 }
