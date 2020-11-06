e3b3aeaa3bb4f5119008d342407ac89a264ce72f
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-05 20:51:37 -0500

    Removed remnants of analysis from functional testing
    Analysis moved to ApexVisual.F1_2020 nuget package

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -3,7 +3,6 @@ using Codemasters.F1_2020;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using Codemasters.F1_2020.Analysis;
 using TimHanewich.Toolkit;
 using System.Threading.Tasks;
 
@@ -13,7 +12,7 @@ namespace FunctionalTesting
     {
         static void Main(string[] args)
         {
-            TrackDataContainer tdc = TrackDataContainer.LoadTrack(Track.AbuDhabi);
+            
         }
     }
 }
