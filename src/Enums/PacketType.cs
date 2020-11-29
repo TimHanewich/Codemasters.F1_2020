58ff53f95d35f7019bdf8761da0e1fb7b486ee78
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-29 15:49:48 -0500

    Added index to PacketType enum

--- src/Enums/PacketType.cs
+++ src/Enums/PacketType.cs
@@ -4,15 +4,15 @@ namespace Codemasters.F1_2020
 {
     public enum PacketType
     {
-        Motion, //1464 bytes
-        Session, //251 bytes
-        Lap, //1190 bytes
-        Event, //35 bytes
-        Participants, //1213 bytes
-        CarSetup, //1102 bytes
-        CarTelemetry, //1307 bytes
-        CarStatus, //1344 bytes
-        FinalClassification, //839 bytes    New to F1 2020.
-        LobbyInfo //1169 bytes   New to F1 2020
+        Motion = 0, //1464 bytes
+        Session = 1, //251 bytes
+        Lap = 2, //1190 bytes
+        Event = 3, //35 bytes
+        Participants = 4, //1213 bytes
+        CarSetup = 5, //1102 bytes
+        CarTelemetry = 6, //1307 bytes
+        CarStatus = 7, //1344 bytes
+        FinalClassification = 8, //839 bytes    New to F1 2020.
+        LobbyInfo = 9 //1169 bytes   New to F1 2020
     }
 }
